5aafe3b84102a492e59cb90fd5eee4f8f8e0da09
franciscohcf <franciscocavalcantefilho@gmail.com>
2021-05-04 08:24:40 -0300

    Inicio de enumeração

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,7 +1,15 @@
 package application;
 
+import entities.Order;
+import entities.enums.OrderStatus;
+
+import java.util.Date;
+
 public class App {
     public static void main(String[] args) {
-        
+
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+        System.out.println(order);
+
     }
 }
